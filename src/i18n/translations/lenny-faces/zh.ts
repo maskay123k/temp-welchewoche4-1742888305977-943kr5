a07140b19f41aeacf0a4d3aff8fd0a651110bc1c
maskay123k <maskay123k@gmail.com>
2025-03-25 08:40:48 +0100

    Copy src/i18n/translations/lenny-faces/zh.ts from welchewoche4

---
+++ src/i18n/translations/lenny-faces/zh.ts
@@ -0,0 +1,27 @@
+export const lennyFacesTranslationsZh = {
+  title: 'Lenny表情',
+  subtitle: '点击Lenny表情复制到剪贴板！',
+  categories: {
+    happy: '开心',
+    sad: '悲伤',
+    angry: '愤怒',
+    love: '爱情',
+    misc: '其他'
+  },
+  faces: {
+    'Classic Lenny': '经典Lenny',
+    'Cool Lenny': '酷炫Lenny',
+    'Happy Lenny': '开心Lenny',
+    'Winking Lenny': '眨眼Lenny',
+    'Sad Lenny': '悲伤Lenny',
+    'Crying Lenny': '哭泣Lenny',
+    'Angry Lenny': '愤怒Lenny',
+    'Table Flip Lenny': '掀桌Lenny',
+    'Love Lenny': '恋爱Lenny',
+    'Heart Eyes': '爱心眼',
+    'Hugging Face': '拥抱脸',
+    'Shrug': '耸肩',
+    'Strong Lenny': '强壮Lenny',
+    'Magic Lenny': '魔法Lenny'
+  }
+} as const;
