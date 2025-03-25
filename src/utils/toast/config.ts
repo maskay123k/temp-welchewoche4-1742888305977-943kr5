711e213370c15a4c8d13ecfa020dddb8a412371e
maskay123k <maskay123k@gmail.com>
2025-03-25 08:44:19 +0100

    Copy src/utils/toast/config.ts from welchewoche4

---
+++ src/utils/toast/config.ts
@@ -0,0 +1,8 @@
+// Toast configuration
+    export const toastConfig = {
+      duration: 2000,
+      classes: {
+        visible: 'translate-y-0 opacity-100',
+        hidden: 'translate-y-full opacity-0'
+      }
+    };
