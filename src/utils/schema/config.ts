ce5e1225bde85ddf19f057de626ddc96b7875d55
maskay123k <maskay123k@gmail.com>
2025-03-25 08:41:40 +0100

    Copy src/utils/schema/config.ts from welchewoche4

---
+++ src/utils/schema/config.ts
@@ -0,0 +1,17 @@
+// Schema.org configuration and constants
+export const schemaConfig = {
+  organization: {
+    name: 'Smileys.lol',
+    url: 'https://smileys.lol',
+    logo: 'https://smileys.lol/logo.png',
+    sameAs: [
+      'https://twitter.com/smileys_lol',
+      'https://github.com/smileys-lol'
+    ]
+  },
+  creator: {
+    name: 'Martin Kreitl',
+    jobTitle: 'Developer',
+    url: 'https://smileys.lol'
+  }
+};
