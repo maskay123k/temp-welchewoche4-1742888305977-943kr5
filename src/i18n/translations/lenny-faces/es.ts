6ef9984c48fa5d56f90d06b84ca4183949e87685
maskay123k <maskay123k@gmail.com>
2025-03-25 08:40:34 +0100

    Copy src/i18n/translations/lenny-faces/es.ts from welchewoche4

---
+++ src/i18n/translations/lenny-faces/es.ts
@@ -0,0 +1,27 @@
+export const lennyFacesTranslationsEs = {
+  title: 'Caras Lenny',
+  subtitle: '¡Haz clic en una Cara Lenny para copiarla al portapapeles!',
+  categories: {
+    happy: 'Feliz',
+    sad: 'Triste',
+    angry: 'Enojado',
+    love: 'Amor',
+    misc: 'Misceláneos'
+  },
+  faces: {
+    'Classic Lenny': 'Lenny Clásico',
+    'Cool Lenny': 'Lenny Cool',
+    'Happy Lenny': 'Lenny Feliz',
+    'Winking Lenny': 'Lenny Guiñando',
+    'Sad Lenny': 'Lenny Triste',
+    'Crying Lenny': 'Lenny Llorando',
+    'Angry Lenny': 'Lenny Enojado',
+    'Table Flip Lenny': 'Lenny Volteando Mesa',
+    'Love Lenny': 'Lenny Enamorado',
+    'Heart Eyes': 'Ojos de Corazón',
+    'Hugging Face': 'Cara Abrazando',
+    'Shrug': 'Encogimiento de Hombros',
+    'Strong Lenny': 'Lenny Fuerte',
+    'Magic Lenny': 'Lenny Mágico'
+  }
+} as const;
