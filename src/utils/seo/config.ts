5df52831c31fb6611557a5bd800b01b4ed90b7c8
maskay123k <maskay123k@gmail.com>
2025-03-25 08:41:47 +0100

    Copy src/utils/seo/config.ts from welchewoche4

---
+++ src/utils/seo/config.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+      name: 'welche-woche.de',
+      description: 'View the current calendar week',
+      url: 'https://welche-woche.de',
+      ogImage: 'https://welche-woche.de/og-image.png',
+      links: {
+        twitter: 'https://twitter.com/welche_woche',
+        github: 'https://github.com/smileys-lol'
+      }
+    };
