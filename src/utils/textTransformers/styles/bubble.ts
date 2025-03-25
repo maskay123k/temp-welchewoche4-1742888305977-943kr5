21da61866d7439f342476d3e3fb8df3e48aca478
maskay123k <maskay123k@gmail.com>
2025-03-25 08:42:42 +0100

    Copy src/utils/textTransformers/styles/bubble.ts from welchewoche4

---
+++ src/utils/textTransformers/styles/bubble.ts
@@ -0,0 +1,15 @@
+import type { StyleDefinition } from '../types';
+
+const bubbleMap: Record<string, string> = {
+  a: 'ⓐ', b: 'ⓑ', c: 'ⓒ', d: 'ⓓ', e: 'ⓔ', f: 'ⓕ', g: 'ⓖ', h: 'ⓗ', i: 'ⓘ', j: 'ⓙ',
+  k: 'ⓚ', l: 'ⓛ', m: 'ⓜ', n: 'ⓝ', o: 'ⓞ', p: 'ⓟ', q: 'ⓠ', r: 'ⓡ', s: 'ⓢ', t: 'ⓣ',
+  u: 'ⓤ', v: 'ⓥ', w: 'ⓦ', x: 'ⓧ', y: 'ⓨ', z: 'ⓩ',
+  A: 'Ⓐ', B: 'Ⓑ', C: 'Ⓒ', D: 'Ⓓ', E: 'Ⓔ', F: 'Ⓕ', G: 'Ⓖ', H: 'Ⓗ', I: 'Ⓘ', J: 'Ⓙ',
+  K: 'Ⓚ', L: 'Ⓛ', M: 'Ⓜ', N: 'Ⓝ', O: 'Ⓞ', P: 'Ⓟ', Q: 'Ⓠ', R: 'Ⓡ', S: 'Ⓢ', T: 'Ⓣ',
+  U: 'Ⓤ', V: 'Ⓥ', W: 'Ⓦ', X: 'Ⓧ', Y: 'Ⓨ', Z: 'Ⓩ'
+};
+
+export const bubbleStyle: StyleDefinition = {
+  name: 'Bubble',
+  transform: (text: string) => text.split('').map(char => bubbleMap[char] || char).join('')
+};
