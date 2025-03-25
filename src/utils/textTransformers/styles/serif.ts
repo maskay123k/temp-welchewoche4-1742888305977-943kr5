fd8fe31c227c02498ef7b37dd6a8a1137566cedd
maskay123k <maskay123k@gmail.com>
2025-03-25 08:43:40 +0100

    Copy src/utils/textTransformers/styles/serif.ts from welchewoche4

---
+++ src/utils/textTransformers/styles/serif.ts
@@ -0,0 +1,15 @@
+import type { StyleDefinition } from '../types';
+
+const serifMap: Record<string, string> = {
+  a: '𝐚', b: '𝐛', c: '𝐜', d: '𝐝', e: '𝐞', f: '𝐟', g: '𝐠', h: '𝐡', i: '𝐢', j: '𝐣',
+  k: '𝐤', l: '𝐥', m: '𝐦', n: '𝐧', o: '𝐨', p: '𝐩', q: '𝐪', r: '𝐫', s: '𝐬', t: '𝐭',
+  u: '𝐮', v: '𝐯', w: '𝐰', x: '𝐱', y: '𝐲', z: '𝐳',
+  A: '𝐀', B: '𝐁', C: '𝐂', D: '𝐃', E: '𝐄', F: '𝐅', G: '𝐆', H: '𝐇', I: '𝐈', J: '𝐉',
+  K: '𝐊', L: '𝐋', M: '𝐌', N: '𝐍', O: '𝐎', P: '𝐏', Q: '𝐐', R: '𝐑', S: '𝐒', T: '𝐓',
+  U: '𝐔', V: '𝐕', W: '𝐖', X: '𝐗', Y: '𝐘', Z: '𝐙'
+};
+
+export const serifStyle: StyleDefinition = {
+  name: 'Serif',
+  transform: (text: string) => text.split('').map(char => serifMap[char] || char).join('')
+};
