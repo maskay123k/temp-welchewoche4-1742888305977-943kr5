ec408bf1126b38df65c7f565f18720caaf76115b
maskay123k <maskay123k@gmail.com>
2025-03-25 08:40:47 +0100

    Copy src/i18n/translations/lenny-faces/ru.ts from welchewoche4

---
+++ src/i18n/translations/lenny-faces/ru.ts
@@ -0,0 +1,27 @@
+export const lennyFacesTranslationsRu = {
+  title: 'Ленни-фейсы',
+  subtitle: 'Нажмите на Ленни-фейс, чтобы скопировать его в буфер обмена!',
+  categories: {
+    happy: 'Счастливый',
+    sad: 'Грустный',
+    angry: 'Злой',
+    love: 'Любовь',
+    misc: 'Разное'
+  },
+  faces: {
+    'Classic Lenny': 'Классический Ленни',
+    'Cool Lenny': 'Крутой Ленни',
+    'Happy Lenny': 'Счастливый Ленни',
+    'Winking Lenny': 'Подмигивающий Ленни',
+    'Sad Lenny': 'Грустный Ленни',
+    'Crying Lenny': 'Плачущий Ленни',
+    'Angry Lenny': 'Злой Ленни',
+    'Table Flip Lenny': 'Ленни Переворачивает Стол',
+    'Love Lenny': 'Влюбленный Ленни',
+    'Heart Eyes': 'Глаза-Сердечки',
+    'Hugging Face': 'Обнимающее Лицо',
+    'Shrug': 'Пожимание Плечами',
+    'Strong Lenny': 'Сильный Ленни',
+    'Magic Lenny': 'Магический Ленни'
+  }
+} as const;
