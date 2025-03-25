fcf791d7c665265534f206725ae4132e055c0761
maskay123k <maskay123k@gmail.com>
2025-03-25 08:40:41 +0100

    Copy src/i18n/translations/lenny-faces/nl.ts from welchewoche4

---
+++ src/i18n/translations/lenny-faces/nl.ts
@@ -0,0 +1,27 @@
+export const lennyFacesTranslationsNl = {
+  title: 'Lenny Gezichten',
+  subtitle: 'Klik op een Lenny Gezicht om het naar het klembord te kopiëren!',
+  categories: {
+    happy: 'Blij',
+    sad: 'Verdrietig',
+    angry: 'Boos',
+    love: 'Liefde',
+    misc: 'Diversen'
+  },
+  faces: {
+    'Classic Lenny': 'Klassieke Lenny',
+    'Cool Lenny': 'Coole Lenny',
+    'Happy Lenny': 'Blije Lenny',
+    'Winking Lenny': 'Knipoogende Lenny',
+    'Sad Lenny': 'Verdrietige Lenny',
+    'Crying Lenny': 'Huilende Lenny',
+    'Angry Lenny': 'Boze Lenny',
+    'Table Flip Lenny': 'Tafel Flip Lenny',
+    'Love Lenny': 'Verliefde Lenny',
+    'Heart Eyes': 'Hart Ogen',
+    'Hugging Face': 'Knuffelend Gezicht',
+    'Shrug': 'Schouderophalen',
+    'Strong Lenny': 'Sterke Lenny',
+    'Magic Lenny': 'Magische Lenny'
+  }
+} as const;
