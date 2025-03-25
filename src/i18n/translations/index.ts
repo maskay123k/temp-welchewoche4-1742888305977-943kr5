483a02f0ab852fb8c9a1d60b09f0ad1788863658
maskay123k <maskay123k@gmail.com>
2025-03-25 08:40:29 +0100

    Copy src/i18n/translations/index.ts from welchewoche4

---
+++ src/i18n/translations/index.ts
@@ -0,0 +1,8 @@
+export * from './navigation';
+    export * from './pages/about';
+    export * from './pages/contact';
+    export * from './pages/imprint';
+    export * from './pages/privacy';
+    
+    // Re-export all translations
+    export { translations } from './translations';
