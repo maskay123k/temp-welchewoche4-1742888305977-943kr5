400cbf16ad8b2c5088197957f8033dd01ed16e91
maskay123k <maskay123k@gmail.com>
2025-03-25 08:43:43 +0100

    Copy src/utils/textTransformers/styles/smallcaps.ts from welchewoche4

---
+++ src/utils/textTransformers/styles/smallcaps.ts
@@ -0,0 +1,12 @@
+import type { StyleDefinition } from '../types';
+
+const smallCapsMap: Record<string, string> = {
+  a: 'ᴀ', b: 'ʙ', c: 'ᴄ', d: 'ᴅ', e: 'ᴇ', f: 'ғ', g: 'ɢ', h: 'ʜ', i: 'ɪ', j: 'ᴊ',
+  k: 'ᴋ', l: 'ʟ', m: 'ᴍ', n: 'ɴ', o: 'ᴏ', p: 'ᴘ', q: 'ǫ', r: 'ʀ', s: 's', t: 'ᴛ',
+  u: 'ᴜ', v: 'ᴠ', w: 'ᴡ', x: 'x', y: 'ʏ', z: 'ᴢ'
+};
+
+export const smallCapsStyle: StyleDefinition = {
+  name: 'Small Caps',
+  transform: (text: string) => text.split('').map(char => smallCapsMap[char.toLowerCase()] || char).join('')
+};
