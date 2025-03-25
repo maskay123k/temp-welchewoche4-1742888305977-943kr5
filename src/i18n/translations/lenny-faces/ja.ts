d750c179a654ccddfefb7b96665a1bc7f6f6e187
maskay123k <maskay123k@gmail.com>
2025-03-25 08:40:37 +0100

    Copy src/i18n/translations/lenny-faces/ja.ts from welchewoche4

---
+++ src/i18n/translations/lenny-faces/ja.ts
@@ -0,0 +1,27 @@
+export const lennyFacesTranslationsJa = {
+  title: 'レニーフェイス',
+  subtitle: 'レニーフェイスをクリックしてクリップボードにコピーしてください！',
+  categories: {
+    happy: '嬉しい',
+    sad: '悲しい',
+    angry: '怒っている',
+    love: '愛',
+    misc: 'その他'
+  },
+  faces: {
+    'Classic Lenny': 'クラシックレニー',
+    'Cool Lenny': 'クールレニー',
+    'Happy Lenny': 'ハッピーレニー',
+    'Winking Lenny': 'ウインクレニー',
+    'Sad Lenny': '悲しいレニー',
+    'Crying Lenny': '泣いているレニー',
+    'Angry Lenny': '怒っているレニー',
+    'Table Flip Lenny': 'テーブルフリップレニー',
+    'Love Lenny': '恋するレニー',
+    'Heart Eyes': 'ハートアイズ',
+    'Hugging Face': 'ハグする顔',
+    'Shrug': '肩をすくめる',
+    'Strong Lenny': '強いレニー',
+    'Magic Lenny': '魔法のレニー'
+  }
+} as const;
